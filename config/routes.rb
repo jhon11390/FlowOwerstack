1f78405319a87dac6c5726bdda911d070a3a492a
jhon11390 <meljhon2@gmail.com>
2020-05-28 05:17:50 -0500

    Agrega modelo Answer y crea la relacion de pregunta con muchas respuestas

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'questions#index'
-  resources :questions, except: [:edit, :update, :destroy]
+  resources :questions, except: [:edit, :update, :destroy] do
+    resources :answers, only: [:create]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_060439) do
+ActiveRecord::Schema.define(version: 2020_05_28_090716) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.integer "question_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
@@ -32,4 +42,6 @@ ActiveRecord::Schema.define(version: 2020_05_28_060439) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
 end
