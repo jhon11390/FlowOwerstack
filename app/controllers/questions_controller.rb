9a4cc35f806c51684938b6ede25d107d1a1da708
jhon11390 <meljhon2@gmail.com>
2020-05-27 23:32:02 -0500

    Agrega vista de crear pregunta en la views/questions/new.html.erb y modifica el archivo routes.rb agregando excepiciones de resources

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,23 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+        redirect_to :questions, notice: "La pregunta fue publicada con éxito"
+    else
+        render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
